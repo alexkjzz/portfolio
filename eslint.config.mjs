4217d1290e9ef2c82341b207b7509d8f75976cdb
alexkjzz <alex.lam@orange.fr>
2025-04-25 17:27:36 +0200

    eslint update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,13 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      'react/no-unescaped-entities': 'off', // Désactiver la règle sur les entités non échappées
+      '@next/next/no-img-element': 'off',   // Désactiver le warning pour <img>
+      '@typescript-eslint/no-unused-vars': 'off', // Désactiver le warning pour les variables inutilisées
+    },
+  },
 ];
 
 export default eslintConfig;
